02cde34b940dfd03b08045101254f6bde295a51f
AlicaWalker2004 <daniella@email.com>
2024-03-26 16:48:19 +0400

    practic day 1

---
+++ Windows/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using Pachkov_Auth.Windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.EntityFrameworkCore;
+using Pachkov_Auth.Classes;
+
+namespace Pachkov_Auth
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RegBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Registr reg = new Registr();
+            reg.Show();
+            this.Hide();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Environment.Exit(0); 
+        }
+
+        private void Voiti_Click(object sender, RoutedEventArgs e)
+        {
+            var login = LoginBox.Text;
+            var pass = PassBox.Text;
+
+            var context = new AppDbContext();
+
+            var user = context.Users.SingleOrDefault(x => x.Login == login && x.Password == pass);
+            if (user is null)
+            {
+                MessageBox.Show("Неправильный логин или пароль!");
+                return;
+            }
+            else
+            {
+                MessageBox.Show("Вы успешно вошли в аккаунт!");
+                Window1 Win1 = new Window1();
+                Win1.Show();
+                this.Hide();
+            }
+
+        }
+    }
+}
---
+++ Windows/Registr.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Pachkov_Auth.Classes;
+using Pachkov_Auth.Windows;
+
+namespace Pachkov_Auth.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для Registr.xaml
+    /// </summary>
+    public partial class Registr : Window
+    {
+        public Registr()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow auth = new MainWindow();
+            auth.Show();
+            this.Hide();
+        }
+
+        private void RegBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var login = LoginBox.Text;
+
+            var pass = PassBox.Text;
+
+            var mail = MailBox.Text;
+
+            var context = new AppDbContext();
+
+            var user_exists = context.Users.FirstOrDefault(x => x.Login == login);
+            if (user_exists is not null)
+            {
+                MessageBox.Show("Такой пользователь уже есть!");
+                return;
+            }
+            var user = new User { Login = login, Password = pass, Email = mail };
+            context.Users.Add(user);
+            context.SaveChanges();
+            MessageBox.Show("Ну привет!");
+        }
+    }
+}
